40b92629b6f094d04c5c0fe20ade7ab4061bf4fb
fabricio-joao <fabriciojoao41@outlook.com>
2023-01-12 10:05:40 -0300

    Estudo sobre requisiçoes http's

---
+++ src/main/java/estudoGet/app/Programa.java
@@ -0,0 +1,30 @@
+package estudoGet.app;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+public class Programa {
+
+	public static final String URL_GET = "http://httpbin.org/get";
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
+	
+		HttpClient cliente = HttpClient.newHttpClient();
+		
+		HttpRequest requisicao = HttpRequest.newBuilder().GET()
+				.timeout(Duration.ofSeconds(10)).uri(URI.create(URL_GET))
+				.build();
+		HttpResponse<String> resposta = cliente.send(requisicao, HttpResponse.BodyHandlers.ofString());
+		
+		System.out.println(resposta.statusCode());
+		System.out.println(resposta.body());
+		
+		
+
+	}
+
+}
